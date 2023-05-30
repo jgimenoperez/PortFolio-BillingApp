5bdfda482f717901bb02385ed644cc8bad7c1dd7
jgimenoperez <jgimenoperez@gmail.com>
2023-05-30 09:35:46 +0200

    parallax handamade

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,3 +1,6 @@
-export * from './Section1.jsx'
-export * from './Section2'
-export * from './Section3'
+import { Section1 } from './Section1'
+import { Section2 } from './Section2'
+import { Section3 } from './Section3'
+
+
+export { Section1, Section2, Section3 };
